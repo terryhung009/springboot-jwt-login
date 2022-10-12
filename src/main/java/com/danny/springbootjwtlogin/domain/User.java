31949f5d6241d96e415e6835c49ad6bbb7fd7895
Denny <khc741213@gmail.com>
2022-10-13 02:07:57 +0800

    1

--- src/main/java/com/danny/springbootjwtlogin/domain/User.java
+++ src/main/java/com/danny/springbootjwtlogin/domain/User.java
@@ -1,22 +1,22 @@
 package com.danny.springbootjwtlogin.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
-@Entity @Data @NoArgsConstructor
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
 public class User {
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String name;
   private String username;
   private String password;
   @ManyToMany(fetch = FetchType.EAGER)
-  private Collection<Role> roles =new ArrayList<>();
+  private Collection<Role> roles = new ArrayList<>();
 
 
 
